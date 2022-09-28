c1b36f2408ea8bf805979efb61de0cea28b7230c
wiratama13 <ashidiqinasrulloh@gmail.com>
2022-09-29 01:14:25 +0700

    menambah paragraph

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga officiis eius qui, doloremque, repellat perferendis itaque autem quisquam distinctio ipsum blanditiis, quidem exercitationem! Corrupti numquam illum earum similique architecto atque, ut maxime tempora quos nihil temporibus voluptatibus tenetur sequi iusto praesentium? Excepturi ullam delectus iusto, vero debitis nulla nam, totam vel, minima commodi quae nisi? Aliquam blanditiis incidunt modi, velit similique vel dignissimos. Veniam, earum consequatur commodi vero incidunt laboriosam, deleniti architecto labore, quo quos aperiam temporibus ad cum blanditiis natus consectetur iste repellat! In possimus iure fugiat sint consectetur dolores! Tenetur, fuga debitis. Reiciendis eligendi vero laudantium. Vero et earum sapiente quibusdam adipisci fugiat odio doloremque quo dignissimos. Perspiciatis corporis vel libero reprehenderit sequi. Nam esse voluptatibus sit voluptate.</p>
 </body>
 </html>
